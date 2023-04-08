175c60343af71458cf3aeecbb07e8c257f1da93c
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-08 18:57:16 -0400

    Eureka Server and Open Feign

--- src/main/java/com/williamfeliciano/employeeservice/service/APIClient.java
+++ src/main/java/com/williamfeliciano/employeeservice/service/APIClient.java
@@ -4,8 +4,10 @@ import com.williamfeliciano.employeeservice.dto.DeparmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-@FeignClient(url="http://localhost:8080", value = "DEPARTMENT-SERVICE")
+// Manual way of connecting to other services
+//@FeignClient(url="http://localhost:8080", value = "DEPARTMENT-SERVICE")
+// eureka automation to connect to other services
+@FeignClient(name="DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DeparmentDto getDepartment(@PathVariable("department-code") String department);
