2327f002896b22beb31603ae32ace6f4d9f643f4
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-08 23:53:26 -0400

    Circuit-Breaker and Retry patterns implementations

--- src/main/java/com/williamfeliciano/employeeservice/service/Impl/EmployeeServiceImpl.java
+++ src/main/java/com/williamfeliciano/employeeservice/service/Impl/EmployeeServiceImpl.java
@@ -8,12 +8,16 @@ import com.williamfeliciano.employeeservice.entity.Employee;
 import com.williamfeliciano.employeeservice.repository.EmployeeRepository;
 import com.williamfeliciano.employeeservice.service.APIClient;
 import com.williamfeliciano.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -22,6 +26,8 @@ import java.util.Optional;
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     //private RestTemplate restTemplate;
 
@@ -41,8 +47,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         return modelMapper.map(employee,EmployeeDto.class);
     }
 
+    //@CircuitBreaker(name="${spring.application.name}",fallbackMethod = "getDefaultDepartment")
+    @Retry(name="${spring.application.name}",fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+        LOGGER.info("Inside getEmployeeByID method");
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(()-> new RuntimeException("Could not find employee"));
         EmployeeDto employeeDto = modelMapper.map(employee,EmployeeDto.class);
 //        // make the api call with the rest template
@@ -65,4 +74,28 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return apiResponseDto;
     }
+
+    public APIResponseDto getDefaultDepartment(Long employeeId,Exception exception){
+        LOGGER.info("Inside getDefaultDepartment method");
+        // Get Employyee
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(()-> new RuntimeException("Could not find employee"));
+
+        // Get EmployeeDto
+        EmployeeDto employeeDto = modelMapper.map(employee,EmployeeDto.class);
+
+        // Create default deparment
+        DeparmentDto deparmentDto = new DeparmentDto();
+        deparmentDto.setDepartmentName("R&D Deparment");
+        deparmentDto.setDepartmentCode("RD001");
+        deparmentDto.setDepartmentDescription("Research and Development Department");
+
+
+        // Create the Responsedto
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDeparment(deparmentDto);
+
+        return apiResponseDto;
+    }
+
 }
